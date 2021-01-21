62a8eb43072d0e48a657595c5a16d6bde844bbbf
gianRVN <gianmohar@gmail.com>
2021-01-21 21:58:58 +0700

    gian

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,44 +1,56 @@
 <template>
-  <div class="home container">
-    <img
-      src="https://cdn.dribbble.com/users/1061278/screenshots/12543160/media/7cd7ab7630ede8e6cb754180c4e46699.png?compress=1&resize=400x300"
-    />
-    <h1 style="margin: 10px 10px">WELCOME to LOBBY!</h1>
-    <div class="col-4
-    ">
-      <div class="" style="margin: 30px 30px">
-        <ul class="list-group">
-          <li class="list-group-item">Cras justo odio</li>
-          <li class="list-group-item">Dapibus ac facilisis in</li>
-          <li class="list-group-item">Morbi leo risus</li>
-          <li class="list-group-item">Porta ac consectetur ac</li>
-          <li class="list-group-item">Vestibulum at eros</li>
-        </ul>
-      </div>
-      <div class="card " style="padding: 20px 20px">
-        <p class="text-start">Start aligned text on all viewport sizes.</p>
-        <p class="text-center">Center aligned text on all viewport sizes.</p>
-        <p class="text-end">End aligned text on all viewport sizes.</p>
-        <p class="text-sm-start">
-          Start aligned text on viewports sized SM (small) or wider.
-        </p>
-        <p class="text-md-start">
-          Start aligned text on viewports sized MD (medium) or wider.
-        </p>
-        <p class="text-lg-start">
-          Start aligned text on viewports sized LG (large) or wider.
-        </p>
-        <p class="text-xl-start">
-          Start aligned text on viewports sized XL (extra-large) or wider.
-        </p>
+  <div class="home">
+    <div class="box-head shadow-lg p-3 mb-5 bg-white rounded">
+      <p style="font-size: 64px; margin-bottom: 0px;">Match Words!</p>
+    </div>
+    <div class="d-flex justify-content-center player-box">
+      <div class="player-name shadow-lg p-5 mb-5 bg-white rounded">
+        <form>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label" style="font-size: 32px;">Player Name</label>
+          <br><br>
+          <input type="text" class="form-control" v-model="playerName" required>
+          <br>
+          <button type="submit" class="btn btn-primary btn-lg" style="font-size: 20px;" @click.prevent='goToRooms'>Play Game</button>
+        </div>
+      </form>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  data () {
+    return {
+      playerName: ''
+    }
+  },
+  methods: {
+    goToRooms () {
+      this.$router.push('/room')
+    }
+  },
+  sockets: {
+    init (payload) {
+      console.log(payload)
+    }
+  }
 }
 </script>
+
+<style scoped>
+.box-head{
+  margin-top: 50px;
+  background-color: red
+}
+.player-name{
+  width: 50%;
+}
+.player-box{
+  margin-top: 100px;
+  padding: 20px;
+}
+</style>
