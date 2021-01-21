1af66bd33e7e6c6a4f4b72edce01e753bfccc9eb
abdulhakim2902 <abdulhakim2902@gmail.com>
2021-01-21 21:02:52 +0700

    create-rooms-page

---
+++ client/src/views/Room.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="container">
+    <div class="row mb-5">
+      <button class="btn btn-primary mx-auto w-25">Create Room</button>
+    </div>
+    <div class="row">
+      <div class="col-md-4 mb-4">
+        <div class="card" style="width: 18rem;">
+          <img src="https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Room 1</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Join</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card" style="width: 18rem;">
+          <img src="https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Room 2</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Join</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card" style="width: 18rem;">
+          <img src="https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Room 3</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Join</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card" style="width: 18rem;">
+          <img src="https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Room 4</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Join</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card" style="width: 18rem;">
+          <img src="https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Room 5</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Join</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
