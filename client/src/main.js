568b238a8397b1d90faba3103153ec2a11492863
abdulhakim2902 <abdulhakim2902@gmail.com>
2021-01-22 00:52:52 +0700

    vue-config

--- client/src/main.js
+++ client/src/main.js
@@ -3,9 +3,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import VueSocketIO from 'vue-socket.io'
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: 'http://localhost:3000',
+  vuex: {
+    store,
+    actionPrefix: 'SOCKET_',
+    mutationPrefix: 'SOCKET_'
+  }
+}))
 
 Vue.config.productionTip = false
 
