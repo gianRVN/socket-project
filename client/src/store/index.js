07d16d581092bfd871961c6e46e63fffdcaeb133
abdulhakim2902 <abdulhakim2902@gmail.com>
2021-01-22 00:44:11 +0700

    store-index-vuex

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,11 +5,23 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    data: {
+      kata: '',
+      point: ''
+    }
   },
   mutations: {
+    setInit (state, payload) {
+      state.data.kata = payload.kata
+      state.data.point = payload.point
+    }
   },
   actions: {
-  },
-  modules: {
+    SOCKET_init (context, payload) {
+      context.commit('setInit', payload)
+    },
+    SOCKET_nextQuestion (context, payload) {
+      context.commit('setInit', payload)
+    }
   }
 })
