aadf832260650e6bd85287a96c3350f60a86be19
abdulhakim2902 <abdulhakim2902@gmail.com>
2021-01-22 00:45:34 +0700

    app-js

--- server/app.js
+++ server/app.js
@@ -1,10 +1,32 @@
 const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+const randomWords = require('./randomWords')
+
+let kata = '';
 
 io.on('connection', function (socket) {
-  console.log('socket connect')
-  // socket.emit()
+  kata = randomWords()
+  socket.emit('init', {
+    kata,
+    point: 0
+  })
+
+  socket.on('answerWord', payload => {
+    if (kata.toLowerCase() == payload.kata) {
+      kata = randomWords()
+      socket.emit('nextQuestion', {
+        kata,
+        point: payload.point + 1
+      })
+    } else {
+      kata = randomWords()
+      socket.emit('nextQuestion', {
+        kata,
+        point: payload.point
+      })
+    }
+  })
 })
 
 server.listen(3000, () => {
